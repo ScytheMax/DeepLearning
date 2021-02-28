cb028483fdfcb662934dd77eb4dcc83b003053c8
ScytheMax <maximilian.sari@t-online.de>
2021-02-28 12:18:44 +0100

    DL-13: main Classes Trainer and its inputs NeuralNetwork and Optimizer

---
+++ Classes/Optimizer.py
@@ -0,0 +1,10 @@
+class Optimizer(object):
+
+    def __init__(self,
+                 lr: float = 0.01):
+
+        self.lr = lr
+
+    def step(self) -> None:
+
+        pass
---
+++ Classes/SGD.py
@@ -0,0 +1,18 @@
+import Optimizer
+
+class SGD(Optimizer):
+    '''
+    Stochasitc gradient descent optimizer.
+    '''    
+    def __init__(self,
+                 lr: float = 0.01) -> None:
+
+        super().__init__(lr)
+
+    def step(self):
+
+        for (param, param_grad) in zip(self.net.params(),
+                                       self.net.param_grads()):
+
+            param -= self.lr * param_grad
+
